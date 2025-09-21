92a24b35ea23f08d20e182e8ad1309d39f2a5cbb
KSam001 <samcapital.inc@gmail.com>
2025-09-21 07:05:36 -0700

    Add custom user model with date_of_birth and profile_photo fields

---
+++ advanced_features_and_security/accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
