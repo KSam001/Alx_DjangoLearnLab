000c85eb4f4d1937432d6e92d5630e5839a53510
KSam001 <samcapital.inc@gmail.com>
2025-10-12 07:54:09 -0700

    Blog Project Task 0 FINALIZED: Cleaned conflicting files, verified project structure, and applied migrations.

--- Introduction_to_Django/LibraryProject/CRUD_operations.md
+++
@@ -1,8 +0,0 @@
-# CRUD Operations for Book Model
-
-## Create Operation
-**Command**:
-```python
-from bookshelf.models import Book
-book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
-print(book)
--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name='blog_posts' 
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
     )
 
     def __str__(self):
